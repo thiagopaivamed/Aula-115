db1a6d923a7fd6f3f2369d26c17331a65108ca80
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-21 07:21:54 -0300

    Gerenciamento de erros

---
+++ GerErros/GerErros/Controllers/ErrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerErros.Controllers
+{
+    public class ErrosController : Controller
+    {
+        [HttpGet("Erros/{codigoErro}")]
+        public IActionResult Index(int codigoErro)
+        {
+            return View(codigoErro);
+        }
+    }
+}
